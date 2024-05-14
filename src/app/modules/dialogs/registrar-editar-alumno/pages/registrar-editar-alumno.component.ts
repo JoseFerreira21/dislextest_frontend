818623e86dc4fea64c76ff0480b120dd22d2e0f0
José <JMFerreiraDuarte@proveedor.teco.com.ar>
2024-05-14 17:48:21 -0300

    commit, mejora lista de alumnos del profesor

--- src/app/modules/dialogs/registrar-editar-alumno/pages/registrar-editar-alumno.component.ts
+++ src/app/modules/dialogs/registrar-editar-alumno/pages/registrar-editar-alumno.component.ts
@@ -4,7 +4,6 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { ErrorStateMatcher, MAT_DATE_FORMATS } from '@angular/material/core';
 import * as moment from 'moment';
 
-
 import {
   FormControl,
   FormGroup,
@@ -43,25 +42,24 @@ export const MY_DATE_FORMATS = {
   providers: [{ provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS }],
 })
 export class RegistrarAlumnoComponent implements OnInit {
-  
   formAlumno: FormGroup;
   tituloAccion: string = 'Nuevo';
   botonAccion: string = 'Guardar';
 
   listaAlumnos: Entidad[] = [];
   dataSource = new MatTableDataSource<Entidad>();
-  
+
   aluProfe = {
     entidadId: 0,
-    profesorId: 0  
-  }
-  
+    profesorId: 0,
+  };
+
   constructor(
     private _dialogoReferencia: MatDialogRef<RegistrarAlumnoComponent>,
     private _formBuilder: FormBuilder,
     private _snackBar: MatSnackBar,
     private _entidadService: EntidadService,
-    private _alumnosService : AlumnosService,
+    private _alumnosService: AlumnosService,
     private _tokenService: TokenService,
     private _profesorService: ProfesorService,
     @Inject(MAT_DIALOG_DATA) public dataAlumno: alumnoEntidad
@@ -77,27 +75,56 @@ export class RegistrarAlumnoComponent implements OnInit {
       usuarioId: [],
     });
 
-    this._alumnosService.getAlumnosDelProfesor(1).subscribe({
-    next: (dataResponse) => {
-        console.log(dataResponse);
-        this.listaAlumnos = dataResponse;
-      },
-      error: (e) => {},
-    });
+    let sub: number;
+    sub = this._tokenService.getSub();
+    // Si 'sub' existe, usamos para llamar a los servicios
+    if (sub) {
+      this._profesorService.getProfesorId(sub).subscribe({
+        next: (dataResponse) => {
+          // Verificar si el array de objetos no está vacío
+          if (dataResponse.length > 0) {
+            // Obtener el primer objeto del array y acceder a su propiedad 'id'
+            const idProfesor = dataResponse[0].id;
+            //console.log('ID del profesor:', idProfesor);
+            this.aluProfe.profesorId = idProfesor;
+
+            this._alumnosService.getAlumnosDelProfesor(idProfesor).subscribe({
+              next: (dataResponse) => {
+                //console.log(dataResponse);
+                this.listaAlumnos = dataResponse;
+              },
+              error: (e) => {},
+            });
+          } else {
+            console.error('El array de objetos está vacío.');
+          }
+        },
+        error: (e) => {
+          console.error('Error al obtener el ID del profesor:', e);
+        },
+      });
+    } else {
+      console.error("No se pudo obtener el campo 'sub' del token.");
+    }
   }
 
-  mostrarAlerta(msg: string, accion: string){
+  mostrarAlerta(msg: string, accion: string) {
     this._snackBar.open(msg, accion, {
-      horizontalPosition: "end",
-      verticalPosition:"top",
-      duration: 3000
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000,
     });
   }
 
-  agregarEditarAlumno(){
-     const fechaNacimiento = moment(this.formAlumno.value.fechaNacimiento).format("DD/MM/YYYY");
-     const fechaISO: string = moment(fechaNacimiento, "DD/MM/YYYY").toISOString();
-     const modelo : Entidad = {
+  agregarEditarAlumno() {
+    const fechaNacimiento = moment(
+      this.formAlumno.value.fechaNacimiento
+    ).format('DD/MM/YYYY');
+    const fechaISO: string = moment(
+      fechaNacimiento,
+      'DD/MM/YYYY'
+    ).toISOString();
+    const modelo: Entidad = {
       tipoEntidad: 'AL',
       nombre: this.formAlumno.value.nombre,
       apellido: this.formAlumno.value.apellido,
@@ -106,18 +133,15 @@ export class RegistrarAlumnoComponent implements OnInit {
       direccion: this.formAlumno.value.direccion,
       nroDocumento: this.formAlumno.value.nroDocumento,
       usuarioId: null,
-    }
+    };
 
-    
-    if (this.dataAlumno === null){
+    if (this.dataAlumno === null) {
       this._entidadService.createEntidad(modelo).subscribe({
         next: (data) => {
-          console.log(data);
-          //Acá implementar post al servicio alumnos, con el id del profesor y recuperar id entidad.
           let respuesta: any;
           respuesta = JSON.parse(JSON.stringify(data));
           this.aluProfe.entidadId = respuesta.id;
-          
+
           let sub: number;
           sub = this._tokenService.getSub();
           // Si 'sub' existe, usamos para llamar a los servicios
@@ -130,15 +154,13 @@ export class RegistrarAlumnoComponent implements OnInit {
                   const idProfesor = dataResponse[0].id;
                   //console.log('ID del profesor:', idProfesor);
                   this.aluProfe.profesorId = idProfesor;
-                  
+
                   this._alumnosService.createAlumno(this.aluProfe).subscribe({
-                    next: (data) => {
-                    },
+                    next: (data) => {},
                     error: (e) => {
-                      console.log(this.aluProfe);
-                    }, 
-                  });   
-                
+                      //console.log(this.aluProfe);
+                    },
+                  });
                 } else {
                   console.error('El array de objetos está vacío.');
                 }
@@ -151,11 +173,11 @@ export class RegistrarAlumnoComponent implements OnInit {
             console.error("No se pudo obtener el campo 'sub' del token.");
           }
           //---------------------------------------------------------//
-          this.mostrarAlerta("Alumno fue registrado con éxito!", "Listo")
-          this._dialogoReferencia.close("creado")
+          this.mostrarAlerta('Alumno fue registrado con éxito!', 'Listo');
+          this._dialogoReferencia.close('creado');
         },
         error: (e) => {
-          this.mostrarAlerta("No se pudo registrar el alumno", "Error")
+          this.mostrarAlerta('No se pudo registrar el alumno', 'Error');
           console.log(modelo);
         },
       });
@@ -163,32 +185,31 @@ export class RegistrarAlumnoComponent implements OnInit {
       this._entidadService.updateEntidad(this.dataAlumno.id, modelo).subscribe({
         next: (data) => {
           console.log(data);
-          this.mostrarAlerta("Alumno fue actualizado con éxito!", "Listo")
-          this._dialogoReferencia.close("editado")
+          this.mostrarAlerta('Alumno fue actualizado con éxito!', 'Listo');
+          this._dialogoReferencia.close('editado');
         },
         error: (e) => {
           console.log(modelo);
-          this.mostrarAlerta("No se pudo actualizar el alumno", "Error")
+          this.mostrarAlerta('No se pudo actualizar el alumno', 'Error');
         },
       });
     }
- 
   }
-  
+
   ngOnInit(): void {
-    if (this.dataAlumno){
+    if (this.dataAlumno) {
       console.log(this.dataAlumno);
       this.formAlumno.patchValue({
         nombre: this.dataAlumno.nombre,
         apellido: this.dataAlumno.apellido,
-        fechaNacimiento: moment(this.dataAlumno.fechaNacimiento , "DD/MM/YYYY"),
+        fechaNacimiento: moment(this.dataAlumno.fechaNacimiento, 'DD/MM/YYYY'),
         telefono: this.dataAlumno.telefono,
         direccion: this.dataAlumno.direccion,
-        nroDocumento: this.dataAlumno.nroDocumento
-      })
+        nroDocumento: this.dataAlumno.nroDocumento,
+      });
 
-      this.tituloAccion = "Editar";
-      this.botonAccion = "Actualizar";
+      this.tituloAccion = 'Editar';
+      this.botonAccion = 'Actualizar';
     }
   }
 }
