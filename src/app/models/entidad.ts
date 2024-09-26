d0f13b14d48118ba64b5f7c95adffad5a3219703
José Ferreira <36089415+JoseFerreira21@users.noreply.github.com>
2024-09-25 20:03:22 -0400

    Mejora e implementación correcta de edición de perfíl de usuario

--- src/app/models/entidad.ts
+++ src/app/models/entidad.ts
@@ -1,5 +1,18 @@
 export interface Entidad {
-    tipoEntidad: string,
+    tipoEntidad?: string,
+    nombre: string,
+    apellido?: string,
+    fechaNacimiento?: string,
+    sexo?: string,
+    telefono?: string,
+    direccion?: string,
+    nroDocumento?: string,
+    usuarioId?: number | null;
+}
+
+export interface EntidadUsuario {
+    id: number,
+    tipoEntidad?: string,
     nombre: string,
     apellido?: string,
     fechaNacimiento?: string,
--- src/app/modules/layout/components/navbar/navbar.component.ts
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -8,6 +8,9 @@ import {
 
 import { AuthService } from '@services/auth.service';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { PerfilComponent } from '../dialogs/perfil/perfil.component'; 
+import { GlobalService } from '@services/global.service';
 
 @Component({
   selector: 'app-navbar',
@@ -22,12 +25,40 @@ export class NavbarComponent {
   isOpenOverlayAvatar = false;
   isOpenOverlayBoards = false;
   showRegister = false;
+  error: string | null = null;
   perfil: any;
+  avatarUrl: string  = '';
 
+  userName: string | undefined; 
+  userEmail: string | undefined; 
+  
   constructor(
+    private dialog: MatDialog,
+    private globalService: GlobalService,
     private authService: AuthService,
     private route: Router,
-  ) {}
+  ) {
+    this.generateRandomAvatar(); // Llama a la función para generar el avatar al cargar el componente
+  }
+
+  generateRandomAvatar() {
+    const randomSeed = Math.random().toString(36).substring(7); // Genera un string aleatorio
+    const avatarId = encodeURIComponent(randomSeed); // Encodea el seed para que sea válido en una URL
+
+    fetch(`https://api.multiavatar.com/${avatarId}.svg`)
+      .then(res => res.text())
+      .then(svg => {
+        this.avatarUrl = 'data:image/svg+xml;base64,' + btoa(svg); // Codifica el SVG en Base64
+      })
+      .catch(err => console.error('Error al generar el avatar:', err));
+  }
+
+  openPerfilModal() {
+    this.dialog.open(PerfilComponent, {
+      width: '600px',
+      disableClose: true
+    });
+  }
 
 
   logout (){
@@ -35,4 +66,14 @@ export class NavbarComponent {
     this.route.navigate(['/login']);
   }
   
+  ngOnInit() {
+    const user = this.authService.getUser();
+    if (user) {
+      this.userName = user.name;
+      this.userEmail = user.email;
+    } else {
+      this.error = 'No se pudo obtener el nombre del usuario.';
+      console.error('No se pudo obtener el nombre del usuario.');
+    }
+  }
 }
